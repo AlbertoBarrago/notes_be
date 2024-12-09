007b57356a888a64ce57b63bb4a9a2bd6c34bbb6
Alberto Barrago <albertobarrago@gmail.com>
2024-12-09 15:53:08 +0100

    Add healthcheck endpoint for service monitoring
    Introduce a new healthcheck endpoint to verify service status. This endpoint returns a JSON response indicating the application's health, facilitating integration with monitoring tools. Updated the main application file to include the healthcheck router for proper routing.

--- app/api/v1/endpoints/__init__.py
+++ app/api/v1/endpoints/__init__.py
@@ -1,13 +1,16 @@
 """
 This module contains the endpoints for the API.
 """
+
 from .auth import router as auth_router
+from .healthcheck import router as healthcheck_router
 from .home import router as home_router
 from .notes import router as notes_router
 from .oauth import router as oauth_router
 from .users import router as users_router
 
 __all__ = (
+	"healthcheck_router",
 	"auth_router",
 	"notes_router",
 	"users_router",
